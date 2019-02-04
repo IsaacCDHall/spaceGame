4feb5bd45d3c74b75e8d024168602d289eb3e238
Matt Groberg and Riley Benson <matt@>
2019-02-04 12:00:20 -0800

    add initial form ui and constructor

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+function Captain(name,story,difficulty){
+  this.name=name;
+  this.story=story;
+  this.difficulty=difficulty;
+  this.funds=0;
+  this.reputation=0;
+  this.food=0;
+  this.population=0;
+}
+
+
+
+$(document).ready(function(){
+  var nameInput= $("#nameInput").val();
+  var storyInput= $("#storyInput").val();
+  var difficultyInput= $("#difficultyInput").val();
+})
